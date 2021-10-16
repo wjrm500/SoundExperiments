93d9282edea87849372c8f546960f7582ec8a485
William May <wjrm500@gmail.com>
2021-10-16 15:13:50 +0800

    Sound experiment 1

---
+++ SoundExperiment1/src/MiniMusicApp.java
@@ -0,0 +1,27 @@
+import javax.sound.midi.*;
+
+public class MiniMusicApp {
+    public static void main(String[] args) {
+        MiniMusicApp mini = new MiniMusicApp();
+        mini.play();
+    }
+
+    public void play() {
+        try {
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+            ShortMessage a = new ShortMessage(144, 1, 44, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            track.add(noteOn);
+            ShortMessage b = new ShortMessage(128, 1, 44, 100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
+            track.add(noteOff);
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+            player.setSequence(seq);
+            player.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
